f5e9f3b66a2acfe6c5e568fc97debfdb2d93f032
Sivabala <sivabala2701@gmail.com>
2026-03-28 20:25:20 +0530

    skills tab changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "web designing", "Data Science", "web development", "Cloud Computing"],
+    strings: ["Backend development", "frontend development", "web designing", "Data Science", "web development", "Cloud Computing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
